32752f82dd4bd5b1bde8f47e62a787a1d0d5df10
kyle <kyle@therich.io>
2024-03-19 18:39:54 +0900

    Update runTest.ts

--- src/commands/runTest.ts
+++ src/commands/runTest.ts
@@ -225,7 +225,6 @@ export async function runTest(
 
 // as of vscode 1.85 ANSI is not fully supported
 const COMMON_ARGS = [
-  "--no-color",
   "--formatter",
   "ElixirLS.DebugAdapter.ExUnitFormatter",
 ];
